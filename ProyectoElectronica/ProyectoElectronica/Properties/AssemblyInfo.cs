cc98cbee0c80b2bfdc783c860f81650eeab45590
michelleayala <57409007+michelleayala@users.noreply.github.com>
2019-11-18 15:53:36 -0500

    Update AssemblyInfo.cs

--- ProyectoElectronica/ProyectoElectronica/Properties/AssemblyInfo.cs
+++ ProyectoElectronica/ProyectoElectronica/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Runtime.InteropServices
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
